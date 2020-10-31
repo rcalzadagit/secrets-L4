e6c5fb714efecd494dd0135133b9793b719de625
RauL CaLzada <rcalzadany@gmail.com>
2020-10-31 16:45:20 -0400

    Updated to use Cookies and Session and Passport module.

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 //Should be in this order
 
 
@@ -15,20 +17,39 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Required by express session package
+app.use(session({
+  secret: "Anylong string goeshere.",
+  resave: false,
+  saveUninitialized: false
+}));
+//See passportjs
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 ////////////////Data/////////////////////////////////////////////////
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
 //User Schema with mongoose-encryption (npm)
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-
+userSchema.plugin(passportLocalMongoose);
 
 //To setup user Model
 const User = new mongoose.model("User", userSchema);
-///////////////////////////////////////////////////////////////////
 
+passport.use(User.createStrategy());
+//Cookie creation
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+///////////////////////////////////////////////////////////////////
+// Viewwing pages
 app.get("/", function(req, res){
   res.render("home");
 });
@@ -41,44 +62,58 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
+app.get("/secrets", function(req, res){
+  if (req.isAuthenticated()){
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+ });
+
+//Route called from the Logout button on the secrets page.
+ app.get("/logout", function(req, res){
+   //Passport Logout function
+   req.logout();
+   res.redirect("/");
+ });
+
 //////////////////////////////////////////////////////////////
 // To get new user registration from the form
 app.post("/register", function(req, res){
 
-bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-  const newUser = new User({
-    email: req.body.username,
-    password: hash
-  });
-newUser.save(function(err){
+User.register({username: req.body.username}, req.body.password, function(err, user) {
   if (err) {
     console.log(err);
-  } else {
-    res.render("secrets");
-  }
+    res.redirect("/register");
+} else {
+  passport.authenticate("local") (req, res, function(){
+    res.redirect("/secrets");
+      });
+    }
   });
-});
 });
 /////////////////////////////////////////////////////////////
 // To get Login data from Login Form page.
 app.post("/login", function(req, res){
-  const username = req.body.username;
-  const password = req.body.password;
 
-  User.findOne({email: username}, function(err, foundUser){
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, function(err, result){
-          if (result === true) {
-          res.render("secrets");
-          }
-        });
-      }
-    }
-  });
+const user = new User({
+  username: req.body.username,
+  password: req.body.password
 });
+//Passport login module and Authenticate - Passport module.
+req.login(user, function(err){
+  if (err) {
+    console.log(err);
+  } else {
+    passport.authenticate("local") (req, res, function(){
+      res.redirect("secrets");
+    });
+  }
+});
+});
+
+
+
 
 
 
